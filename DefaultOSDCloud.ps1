7f692f4ac82e07207507b4ef367c21b6a2a0654f
Mark Birchall <mark.birchall@novus.co.uk>
2021-09-08 12:50:42 +0100

    v1.2
    Added Menu with AutoPilotOOBE

--- DefaultOSDCloud.ps1
+++ DefaultOSDCloud.ps1
@@ -6,17 +6,31 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1366x"
     Set-DisRes 1366
 }
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch WIN10 21H1 | en-gb | Enterprise"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch WIN10 20H2 | en-gb | Enterprise" -ForegroundColor Yellow
+Write-Host "3: AutoPilotOOBE | Testing" -ForegroundColor Yellow
+Write-Host "4: Exit`n"-ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
 
 #Make sure I have the latest OSD Content
 Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
 Install-Module OSD -Force
+Install-Module AutopilotOOBE -Force
 
-Write-Host  -ForegroundColor Cyan "Importing PowerShell Module"
+Write-Host  -ForegroundColor Cyan "Importing PowerShell Modules"
 Import-Module OSD -Force
+Import-Module AutopilotOOBE
 
-#Start OSDCloud ZTI
-Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
-Start-OSDCloud -OSLanguage en-gb -OSBuild 20H2 -OSEdition Enterprise -ZTI
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-gb -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-gb -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '3' { AutoPilotOOBE	} 
+    '4' { Exit		}
+}
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
