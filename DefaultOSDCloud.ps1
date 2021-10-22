b5e14dd6a2c1220785b19d19976451dc589b7668
Mark Birchall <mark.birchall@novus.co.uk>
2021-10-22 15:56:41 +0100

    Update DefaultOSDCloud.ps1

--- DefaultOSDCloud.ps1
+++ DefaultOSDCloud.ps1
@@ -1,36 +1,36 @@
-#   OS: Params and Start-OSDCloud
-$Params=@{
-    OSBuild = "21H1"
-    OSEdition = "Enterprise"
-    OSLanguage = "en-gb"
-    OSLicense = "Volume"
-    SkipAutopilot = $true
-    SkipODT = $true
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud ..."
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1366x"
+    Set-DisRes 1366
 }
-Start-OSDCloud @Params
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch WIN10 21H1 | en-gb | Enterprise"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch WIN10 20H2 | en-gb | Enterprise" -ForegroundColor Yellow
+Write-Host "3: OSDCloudGUI | Testing" -ForegroundColor Yellow
+Write-Host "4: Exit" -ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
 
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+Install-Module OSD -Force
 
-#   PostOS: AutopilotOOBE Staging
-$AutopilotOOBEJson=@'
+Write-Host  -ForegroundColor Cyan "Importing PowerShell Modules"
+Import-Module OSD -Force
+
+switch ($input)
 {
-    "Assign":  {
-                   "IsPresent":  true
-               },
-    "GroupTag":  "Enterprise",
-    "GroupTagOptions":  [
-                            "Development",
-                            "Enterprise"
-                        ],
-    "Hidden":  [
-                   "AddToGroup",
-                   "AssignedComputerName",
-                   "AssignedUser",
-                   "PostAction"
-               ],
-    "PostAction":  "Quit",
-    "Run":  "NetworkingWireless",
-    "Docs":  "https://autopilotoobe.osdeploy.com/",
-    "Title":  "OSDeploy Autopilot Registration"
+    '1' { Start-OSDCloud -OSLanguage en-gb -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-gb -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '3' { Start-OSDCloudGUI	} 
+    '4' { Exit }
 }
-'@
-$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE"
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
